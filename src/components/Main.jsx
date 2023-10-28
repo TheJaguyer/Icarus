29e78651287c2686dc9c29593c905c6eb07fbd37
TheJaguyer <guyer.jarrett@gmail.com>
2023-10-28 16:39:59 -0600

    edited one word

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ export default function Main({ text }) {
       <PrimaryText text={"See what critics have to say:"}></PrimaryText>
       <Review
         quote={
-          "When I first received my copy of Alex Weatherhead it was working perfectly. It was making respectable choices and had even graduated from a prestigious university. However I believe my copy may be defective as it rapidly became disinterested in anything useful and spent most of its time in debauchery. I am hopeful that my Alex will start working properly again, as it is now engaged to be married to a talented and intelligent woman which may be a sign that this most recent software update has worked out some of the bugs. Overall, I give this produce two stars. I would change this rating to 4 stars if this recent update ends up resolving the aforementioned issues."
+          "When I first received my copy of Alex Weatherhead it was working perfectly. It was making respectable choices and had even graduated from a prestigious university. However I believe my copy may be defective as it rapidly became disinterested in anything useful and spent most of its time in debauchery. I am hopeful that my Alex will start working properly again, as it is now engaged to be married to a talented and intelligent woman which may be a sign that this most recent software update has worked out some of the bugs. Overall, I give this product two stars. I would change this rating to 4 stars if this recent update ends up resolving the aforementioned issues."
         }
         person={"Scott McCormack"}
       />

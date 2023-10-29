769a62da6960474b8d301391ae7d0472d2f95959
TheJaguyer <guyer.jarrett@gmail.com>
2023-10-29 15:11:00 -0600

    minor adjustments to animation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import styles from "../styles/page.module.css";
 import Topper from "../components/Topper/Topper";
 import Stopper from "../components/Stopper/Stopper";
 import Main from "../components/Main";
 import Loading from "../components/LoadingScreen/Loading";
+import Splash from "@/components/Splash";
 
 export default function Home() {
   const [passedLoad, setPassedLoad] = useState(false);
 
-  return (
-    <main className={styles.main}>
-      {!passedLoad && <Loading setPassedLoad={setPassedLoad} />}
+  const [loaded, setLoaded] = useState(false);
 
-      <Topper></Topper>
-      <Main />
-      <Stopper></Stopper>
-    </main>
-  );
+  useEffect(() => {
+    setLoaded(true);
+  });
+
+  if (loaded) {
+    return (
+      <main className={styles.main}>
+        {!passedLoad && <Loading setPassedLoad={setPassedLoad} />}
+
+        <Topper></Topper>
+        <Main />
+        <Stopper></Stopper>
+      </main>
+    );
+  } else {
+    return <Splash />;
+  }
 }
---
+++ src/components/Splash.jsx
@@ -0,0 +1,5 @@
+import styles from "@/styles/loading.module.css";
+
+export default function Splash() {
+  return <div className={styles.loading}></div>;
+}

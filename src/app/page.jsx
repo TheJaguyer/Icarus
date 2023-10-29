aa43cc51a58557e8ff220ae90de02ee5350a9e52
TheJaguyer <guyer.jarrett@gmail.com>
2023-10-29 14:09:47 -0600

    added transition from opening anim to main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 import styles from "../styles/page.module.css";
 import Topper from "../components/Topper";
 import Stopper from "../components/Stopper";
@@ -5,9 +9,11 @@ import Main from "../components/Main";
 import Loading from "../components/LoadingScreen/Loading";
 
 export default function Home() {
+  const [passedLoad, setPassedLoad] = useState(false);
+
   return (
     <main className={styles.main}>
-      <Loading />
+      {!passedLoad && <Loading setPassedLoad={setPassedLoad} />}
 
       <Topper></Topper>
       <Main />
--- src/components/LoadingScreen/Loading.jsx
+++ src/components/LoadingScreen/Loading.jsx
@@ -6,15 +6,23 @@ import Icarus from "./Icarus";
 import Daedalus from "./Deadalus";
 import { useState } from "react";
 
-export default function Loading() {
+export default function Loading({ setPassedLoad }) {
   const [touched, setTouched] = useState(false);
+  const [change, setChange] = useState("");
 
   function sunTouched() {
     setTouched(true);
     console.log("sun touched");
+    setTimeout(() => {
+      setChange("fade");
+      console.log("passed load");
+    }, 900);
+    setTimeout(() => {
+      setPassedLoad(true);
+    }, 1200);
   }
   return (
-    <div className={styles.loading}>
+    <div className={`${styles.loading} ${styles[change]}`}>
       <Daedalus touched={touched} />
       <Icarus touched={touched} />
       <Sun sunTouched={sunTouched} touched={touched} />

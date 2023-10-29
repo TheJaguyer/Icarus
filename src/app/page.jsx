6549df6850cd2f8cb0a60f564ecf6a98d550baee
TheJaguyer <guyer.jarrett@gmail.com>
2023-10-29 15:23:05 -0600

    rearranged some divs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,34 +1,22 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import styles from "../styles/page.module.css";
 import Topper from "../components/Topper/Topper";
 import Stopper from "../components/Stopper/Stopper";
 import Main from "../components/Main";
 import Loading from "../components/LoadingScreen/Loading";
-import Splash from "@/components/Splash";
 
 export default function Home() {
   const [passedLoad, setPassedLoad] = useState(false);
 
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    setLoaded(true);
-  });
-
-  if (loaded) {
-    return (
-      <main className={styles.main}>
-        {!passedLoad && <Loading setPassedLoad={setPassedLoad} />}
-
-        <Topper></Topper>
-        <Main />
-        <Stopper></Stopper>
-      </main>
-    );
-  } else {
-    return <Splash />;
-  }
+  return (
+    <main className={styles.main}>
+      <Topper></Topper>
+      <Main />
+      <Stopper></Stopper>
+      {!passedLoad && <Loading setPassedLoad={setPassedLoad} />}
+    </main>
+  );
 }
--- src/components/Splash.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "@/styles/loading.module.css";
-
-export default function Splash() {
-  return <div className={styles.loading}></div>;
-}
